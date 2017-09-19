d30438c445b40f51827239019ac8d40bf8da80d0
scandar <thescandar@gmail.com>
2017-09-19 21:21:20 +0200

    typing notification

--- public/chat.js
+++ public/chat.js
@@ -3,21 +3,39 @@
 var socket = io.connect('http://localhost:3000');
 
 // Query DOM
-var message = document.getElementById('message'),
-    handle  = document.getElementById('handle'),
-    btn     = document.getElementById('send'),
-    output  = document.getElementById('output');
+var message  = document.getElementById('message'),
+    handle   = document.getElementById('handle'),
+    btn      = document.getElementById('send'),
+    output   = document.getElementById('output'),
+    feedback = document.getElementById('feedback');
 
 
 // Emit events
 btn.addEventListener('click', function() {
-    socket.emit('chat', {
-        message: message.value,
-        handle: handle.value
-    });
+    if (message.value != '' && handle.value != '') {
+        socket.emit('chat', {
+            message: message.value,
+            handle: handle.value
+        });
+    }
+});
+
+message.addEventListener('keypress', function () {
+    socket.emit('typing', handle.value);
 });
 
 // listen for events
 socket.on('chat', function (data) {
+    feedback.innerHTML = '';
     output.innerHTML += `<p><strong>${data.handle}: </strong>${data.message}</p>`;
+});
+
+var timer = null;
+socket.on('typing', function (data) {
+    clearTimeout(timer);
+    timer = setTimeout(function () {
+        feedback.innerHTML = '';
+    }, 2000);
+
+    feedback.innerHTML = `<p><em>${data} is typing...</em></p>`;
 });
